473bac05f5b14d41263a31bb81a7d626de879bee
Guerin Yves <yves_guerin@groupevsc.com>
2019-10-28 12:00:25 +0100

    questionnaire

---
+++ src/components/Question.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+    <b-form-group :label="question.enonce">
+      <b-form-checkbox-group id="checkbox-group-2" v-model="selected">
+        <b-form-checkbox v-for="reponse in question.reponses" :key="reponse" :value="reponse">{{ reponse }}</b-form-checkbox>
+      </b-form-checkbox-group>
+    </b-form-group>
+  </div>
+</template>
+
+<script>
+import questions from '../assets/questions.json'
+export default {
+  name: 'YGQuestion',
+  data () {
+    return {
+      selected: []
+    }
+  },
+  props: {
+    questionNumber: {
+      type: Number,
+      required: true
+    }
+  },
+  computed: {
+    question () {
+      return questions.questions[this.questionNumber]
+    }
+  }
+}
+</script>
--- src/components/Questionnaire.vue
+++ src/components/Questionnaire.vue
@@ -1,9 +1,48 @@
 <template>
-    <h1>yo</h1>
+<div>
+    <YGQuestion :questionNumber=question />
+    <b-button variant="primary" :disabled="disable" v-on:click="previous">Question précédente</b-button>
+    <b-button variant="primary" v-on:click="next">Question suivante</b-button>
+</div>
 </template>
 
 <script>
+import YGQuestion from './Question.vue'
+import questions from '../assets/questions.json'
+
 export default {
-  name: 'YGQuestionnaire'
+  name: 'YGQuestionnaire',
+  data: function () {
+    return {
+      question: 0,
+      disable: true
+    }
+  },
+  components: {
+    YGQuestion
+  },
+  computed: {
+    nbQuestions () {
+      return questions.nbQuestions - 1
+    }
+  },
+  methods: {
+    previous (evt) {
+      evt.preventDefault()
+      this.question -= 1
+      if (this.question === 0) {
+        this.disable = true
+      }
+    },
+    next (evt) {
+      evt.preventDefault()
+      if (this.question < this.nbQuestions) {
+        this.question += 1
+      }
+      if (this.question > 0) {
+        this.disable = false
+      }
+    }
+  }
 }
 </script>
